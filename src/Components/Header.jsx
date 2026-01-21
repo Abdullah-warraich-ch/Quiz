2c66291dbe1a30b6882a8fb799be4762dc4ea75b
Abdullah-warraich-ch <abdullahnasar333@gmail.com>
2026-01-21 16:15:44 +0500

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <div>Quiz</div>
+      <div>
+        <a href="#">Login</a>
+        <a href="#">Register</a>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Header;
