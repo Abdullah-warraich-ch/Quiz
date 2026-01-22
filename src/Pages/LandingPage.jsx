028ce5c36cf185b08d877bed0f081ee4df0255b2
Abdullah-warraich-ch <abdullahnasar333@gmail.com>
2026-01-22 14:33:26 +0500

    Property Section Completed

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Components/Header.jsx";
 import Hero from "../Components/Hero.jsx";
+import Properties from "../Components/Properties.jsx";
 function LandingPage() {
   return (
     <div>
       <Header />
       <Hero />
+      <Properties />
     </div>
   );
 }
