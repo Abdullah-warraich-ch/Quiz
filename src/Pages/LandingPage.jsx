06a3a8153753480bc8c5eaa5225331b72329f8b7
Abdullah-warraich-ch <abdullahnasar333@gmail.com>
2026-01-22 16:27:36 +0500

    Filters Completed

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../Components/Header.jsx";
 import Hero from "../Components/Hero.jsx";
 import Properties from "../Components/Properties.jsx";
+import Filter from "../Components/Filter.jsx";
 function LandingPage() {
   return (
     <div>
       <Header />
       <Hero />
       <Properties />
+      <Filter />
     </div>
   );
 }
